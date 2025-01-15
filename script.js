cd58b8736834f4f890eb38a3d0f49288c5296fc9
dovEE3 <170572052+doVEE3@users.noreply.github.com>
2025-01-15 19:47:46 +0100

    Object constructor changed with class constructor

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author; 
-  this.pages = pages; 
-  this.isRead = isRead; 
+class Book{
+  constructor(title, author, pages, isRead) {
+
+    this.title = title;
+    this.author = author; 
+    this.pages = pages; 
+    this.isRead = isRead;     
+  }
 }
 
 Book.prototype.isReadToggle = function() {
